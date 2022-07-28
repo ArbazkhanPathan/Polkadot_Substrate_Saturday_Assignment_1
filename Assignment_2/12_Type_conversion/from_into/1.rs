e0e7ea6ec99d20d4fb0dab158c9fe7dc59c8c6cb
Arbazkhan Pathan <74857530+ArbazkhanPathan@users.noreply.github.com>
2022-07-28 16:45:34 +0530

    type conversion type into

---
+++ Assignment_2/12_Type_conversion/from_into/1.rs
@@ -0,0 +1,33 @@
+// From/Into
+
+// Q1  
+
+fn main() {
+    // impl From<bool> for i32
+   let i1:i32 = false.into();
+   let i2:i32 = i32::from(false);  
+   assert_eq!(i1, i2);
+   assert_eq!(i1, 0);
+
+   let i3: u32 = 'a'.into();
+
+   let s: String = 'a'.into();
+   println!("Success!")
+}
+
+
+/////
+/// 
+/// 
+fn main() {
+     // impl From<bool> for i32
+     let i1:i32 = false.into();
+     let i2:i32 = i32::from(false);  
+     assert_eq!(i1, i2);
+     assert_eq!(i1, 0);
+ 
+     let i3: u32 = 'a' as u32 ;
+ 
+     let s: String = String::from('a');
+     println!("Success")
+ }
