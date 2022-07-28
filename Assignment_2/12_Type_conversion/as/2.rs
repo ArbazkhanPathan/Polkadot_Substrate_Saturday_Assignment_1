42dd5d3aac0e491832246a9cb81a39891e154162
Arbazkhan Pathan <74857530+ArbazkhanPathan@users.noreply.github.com>
2022-07-28 16:38:09 +0530

    type conversion as

---
+++ Assignment_2/12_Type_conversion/as/2.rs
@@ -0,0 +1,11 @@
+// Q2 
+
+// Suppress all warnings from casts which overflow.
+#![allow(overflowing_literals)]
+
+fn main() {
+    assert_eq!(u8::MAX, 255);
+    let v = 1000 as u8;
+
+    println!("Success!")
+}
