a012936e5871b1bf112e69879cc2b9486e801f55
Arbazkhan Pathan <74857530+ArbazkhanPathan@users.noreply.github.com>
2022-07-28 17:19:05 +0530

    crate and Module package and crate

---
+++ Assignment_2/14_crate_and_module/package_and_crate/3.rs
@@ -0,0 +1,5 @@
+/* FILL in the blank with your ANSWER */
+
+// Q: Whats the difference between package 1# and 2# ?
+// A: hello-package has a binary crate named hello-package, src/main.rs is the crate root.
+// hello-pacakge1 has a library crate named hello-package1, src/lib.rs is the crate root.
